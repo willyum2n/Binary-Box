cef2ff625021896c767bf08ff901d41bf3e5cf51
Will Johnson <willyum2n@gmail.com>
2021-04-11 18:25:42 -0400

    Found documentation on how to work with the LCD Display. Updated includes Picked all GPIO pins as inputs from the rocker switches. Will be active low Flushed out LCD printing routine when rocker switches change

--- src/binarybox/binarybox.ino
+++ src/binarybox/binarybox.ino
@@ -7,41 +7,69 @@
 * This program is being written for the ESP32S. Make sure you pick the correct board in the tools menu
 * 
 *******************************************************************/
-//TODO: Don't have I2C LED displays yet. Will need to determine which libraries this display needs
+
+//--------- LCD Display -----------------------------
 //TODO: Detemine with I2C address this display uses. There have been reports that it can be 0x27 or 0x3F
 #include <Wire.h>
-#include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
-#define SW_0 ? //TODO: Pick a pin on the ESP32
-#define SW_1 ? //TODO: Pick a pin on the ESP32
-#define SW_2 ? //TODO: Pick a pin on the ESP32
-#define SW_3 ? //TODO: Pick a pin on the ESP32
-#define SW_4 ? //TODO: Pick a pin on the ESP32
-#define SW_5 ? //TODO: Pick a pin on the ESP32
-#define SW_6 ? //TODO: Pick a pin on the ESP32
-#define SW_7 ? //TODO: Pick a pin on the ESP32
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-byte swValue = 0; // This is the variable that holds all the switch positions
+//--------- Binary Switches -----------------------------
+#define SW_0 13 // 1's Place
+#define SW_1 12 // 2's Place
+#define SW_2 14 // 4's Place
+#define SW_3 27 // 8's Place
+#define SW_4 26 // 16's Place
+#define SW_5 25 // 32's Place
+#define SW_6 33 // 64's Place
+#define SW_7 32 // 128's Place
+
+byte swValue = 0; // This is the variable that holds all the switch positions between loop iterations
 
 void setup() {
     Serial.begin(115200);
 
-    // Init GPIO Inputs coming from Switches
-    //TODO:
+    // Init GPIO Inputs coming from Switches - This is a polling app (No interrupts needed)
+    pinMode(SW_0, INPUT_PULLUP);
+    pinMode(SW_1, INPUT_PULLUP);
+    pinMode(SW_2, INPUT_PULLUP);
+    pinMode(SW_3, INPUT_PULLUP);
+    pinMode(SW_4, INPUT_PULLUP);
+    pinMode(SW_5, INPUT_PULLUP);
+    pinMode(SW_6, INPUT_PULLUP);
+    pinMode(SW_7, INPUT_PULLUP);
     
     // Look for I2C devices
     Serial.println("Scan for devices on I2C bus");
     I2Cscan();
 
-    // TODO: Init LCD Display
+    // Init LCD Display
+    lcd.init();  //initialize the lcd
+    lcd.backlight();  //open the backlight
 }
 
 void loop() {
 
     byte currentValue = 0;
     // Read Switch Values and shift them into our currentValue buffer variable
-    //TODO:
+    Serial.print(digitalRead(SW_7));
+    Serial.print(digitalRead(SW_6));
+    Serial.print(digitalRead(SW_5));
+    Serial.print(digitalRead(SW_4));
+    Serial.print(digitalRead(SW_3));
+    Serial.print(digitalRead(SW_2));
+    Serial.print(digitalRead(SW_1));
+    Serial.println(digitalRead(SW_0));
+    
+    currentValue = (!digitalRead(SW_0) & 0x01) || currentValue;
+    currentValue = (!digitalRead(SW_1) & 0x02) || currentValue;
+    currentValue = (!digitalRead(SW_2) & 0x04) || currentValue;
+    currentValue = (!digitalRead(SW_3) & 0x08) || currentValue;
+    currentValue = (!digitalRead(SW_4) & 0x10) || currentValue;
+    currentValue = (!digitalRead(SW_5) & 0x20) || currentValue;
+    currentValue = (!digitalRead(SW_6) & 0x40) || currentValue;
+    currentValue = (!digitalRead(SW_7) & 0x80) || currentValue;
 
     // Check if the values have changed
     if (currentValue != swValue) {
@@ -103,6 +131,20 @@ void updateDisplay(byte value) {
    * binary: 11111111  
    */
 
-    //TODO: take the value input and turn it into a display string using the following format:
+//    String binStr = String(value, BIN);
+//    String octStr = String(value, OCT);
+//    String decStr = String(value, DEC);
+//    String hexStr = String(value, HEX);
 
+    lcd.setCursor(0, 0); // set the cursor to column 0, line 0
+    lcd.print(value, DEC);
+
+    lcd.setCursor(0,5);
+    lcd.print("0b" + String(value, OCT));
+
+    lcd.setCursor(0,12);
+    lcd.print("0x" + String(value, HEX));
+
+    lcd.setCursor(1,0);
+    lcd.print("binary: " + String(value, BIN));        
 }
